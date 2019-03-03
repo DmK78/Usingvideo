2daf94303b31336da00d20ec4ceda90eb217b808
DmK78 <Ek193burg>
2019-03-03 12:53:23 +0500

    Adding media controller

--- app/src/main/java/com/example/usingvideo/MainActivity.java
+++ app/src/main/java/com/example/usingvideo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.usingvideo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView = findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.videoplayback);
+        videoView.start();
     }
 }
