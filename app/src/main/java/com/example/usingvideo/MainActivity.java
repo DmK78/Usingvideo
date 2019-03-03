53435245618c1ee4be6a1069006746ca558d8293
DmK78 <Ek193burg>
2019-03-03 13:27:10 +0500

    Changing video path

--- app/src/main/java/com/example/usingvideo/MainActivity.java
+++ app/src/main/java/com/example/usingvideo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.usingvideo;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.MediaController;
@@ -13,7 +14,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         VideoView videoView = findViewById(R.id.videoView);
-        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.videoplayback);
+        String videoPath = "android.resource://com.example.usingvideo/"+ R.raw.videoplayback;
+        //String videoPath ="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+        //videoView.setVideoURI(Uri.parse(videoPath));
+
+        videoView.setVideoPath(videoPath);
         MediaController mediaController = new MediaController(this);
         mediaController.setAnchorView(videoView);
         videoView.setMediaController(mediaController);
